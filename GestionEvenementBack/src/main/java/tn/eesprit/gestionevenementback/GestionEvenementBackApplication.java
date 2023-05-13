3623e9e07aa4e0716e5a75c98c6d4168b3cb7d04
ndmtir <ndmtir@gmail.com>
2023-05-14 00:16:19 +0100

    First Commit

---
+++ GestionEvenementBack/src/main/java/tn/eesprit/gestionevenementback/GestionEvenementBackApplication.java
@@ -0,0 +1,13 @@
+package tn.eesprit.gestionevenementback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionEvenementBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionEvenementBackApplication.class, args);
+    }
+
+}
