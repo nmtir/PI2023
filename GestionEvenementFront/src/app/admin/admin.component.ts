9c1a85da98918ef14c6138bbed6ba7c867275a9e
ndmtir <ndmtir@gmail.com>
2023-05-17 17:48:28 +0100

    jdk 20->17

---
+++ GestionEvenementFront/src/app/admin/admin.component.ts
@@ -0,0 +1,24 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.css']
+})
+export class AdminComponent implements OnInit{
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+
+  }
+  //Sidebar toggle show hide function
+  status = false;
+  addToggle()
+  {
+    this.status = !this.status;
+  }
+
+
+}
---
+++ GestionEvenementFront/src/app/home/home.component.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit} from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  imageUrl1: string = './assets/images/speaker-1.jpg';
+  imageUrl2: string = './assets/images/speaker-2.jpg';
+  imageUrl3: string = './assets/images/speaker-3.jpg';
+  imageUrl4: string = './assets/images/speaker-4.jpg';
+  imageUrl5: string = './assets/images/speaker-5.jpg';
+
+}
